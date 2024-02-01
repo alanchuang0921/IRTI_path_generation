4444dc5f74c4e10bdf7e1207fa80a3f382e1b446
Nick20010505 <nick24564625@gmail.com>
2024-02-01 13:26:09 +0800

    0131

--- 程式碼/讀取IGS檔案.py
+++ 程式碼/讀取IGS檔案.py
@@ -1,1 +1,17 @@
-import open3d as o3d
+# import open3d as o3d
+
+# mesh = o3d.io.read_triangle_mesh("D0920.IGS")
+
+# o3d.visualization.draw_geometries([mesh])
+
+import pyiges
+
+print(dir(pyiges))
+print(pyiges.__builtins__)
+
+iges = pyiges.Iges(r"C:\NTHU\IRTI-Project\D0920.IGS")
+
+print(iges)
+
+mesh = iges.bspline_surfaces(as_vtk=True, merge=True)
+mesh.plot()
